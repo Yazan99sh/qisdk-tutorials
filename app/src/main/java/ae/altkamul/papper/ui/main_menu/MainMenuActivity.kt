4b2aaad209b6e21f6cabab2600c8c05b52201159
Yazan <69546196+Yazan99sh@users.noreply.github.com>
2024-10-22 09:54:46 +0400

    ui : enhance ui in welcome message

--- app/src/main/java/ae/altkamul/papper/ui/main_menu/MainMenuActivity.kt
+++ app/src/main/java/ae/altkamul/papper/ui/main_menu/MainMenuActivity.kt
@@ -5,10 +5,12 @@ import ae.altkamul.papper.ui.welcomemessage.WelcomeMessageContract
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aldebaran.qi.sdk.design.activity.RobotActivity
+import com.aldebaran.qi.sdk.design.activity.conversationstatus.SpeechBarDisplayStrategy
 
 class MainMenuActivity : RobotActivity(), MainMenuContract.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setSpeechBarDisplayStrategy(SpeechBarDisplayStrategy.OVERLAY)
         setContentView(R.layout.activity_main_menu)
     }
 }
